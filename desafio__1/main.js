6d500f3ef2d9b35e21b3cf33f0db444196c416e4
jazminpc <jazminpaco1@gmail.com>
2024-05-23 22:43:56 -0300

    desafio 1

---
+++ desafio__1/main.js
@@ -0,0 +1,23 @@
+/* Creá 3 variables:
+
+Una para asignar el nombre del producto (nombreProducto)
+
+Una para el precio por unidad (precioUnitario) con sus respectivos valores 
+
+Y otra que almacenará la cantidad deseada por el usuario (cantidadDeseada) 
+
+Creá un prompt donde se le pregunte al usuario la cantidad que desea comprar y que esa respuesta se almacene en cantidadDeseada.
+
+Luego calcular el costo total de la compra multiplicando el precioUnitario por la cantidadDeseada
+
+Utilizá alert() para informar al usuario sobre el costo total de su compra.*/
+
+let cantidad_desea = prompt("inserte la cantidad de lapiceras que quiere comprar :) :");
+
+let nombre_de_poducto ="lapicera"
+const precio_unitario = 150
+let cantidad_deseada = cantidad_desea
+
+
+let multiplicando = precio_unitario * cantidad_desea;
+alert("El precio en total seria:" + multiplicando)
